5c4e4ebdbdb132cda52f18b8ac550a4dc42d8546
omen <abubakr.sidiqque@gmail.com>
2023-05-23 21:51:24 +0500

    Bug Fix: HomeScreen instance was returning from home-screen.jsx instead of HomeScreen component reference.

--- client/screens/home-screen.jsx
+++ client/screens/home-screen.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-function HomeScreen() {
+const HomeScreen = (props) => {
     return (
         <View style={ { flex: 1, alignItems: 'center', justifyContent: 'center' } }>
             <Text>Home Screen</Text>
         </View>
     );
-}
+};
 
-export default HomeScreen();
+export default HomeScreen;
